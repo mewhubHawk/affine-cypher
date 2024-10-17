2c27ab882dde06a4cd5aab3fcc15551f9b4c3a8b
Kriemhild Gretchen <34939082+mewhubHawk@users.noreply.github.com>
2024-10-17 21:05:51 +0100

    Create affine-cypher.py

---
+++ affine-cypher.py
@@ -0,0 +1,31 @@
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#index = 0
+words = [
+  "babbage",
+  "charles",
+  "ada",
+  "lovelace",
+  "kate",
+  "warne",
+  "bullet"
+]
+
+print(alphabet[0])
+
+for index in range(26):
+  print(alphabet[index])
+
+from random import randint
+
+index = randint
+if letter > 26 
+   letter - 26
+
+'''
+cypher = input("Enter what you'd like to be decyphered: ")
+
+each letter refers to a number
+
+if letter > 26 
+   letter - 26
+'''
